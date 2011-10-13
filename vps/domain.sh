ae8dee91c835744f3300e64ea3fea6616d93c45a
TigersWay <projects@tigersway.net>
2011-10-13 14:43:58 +0700

    PHP5-FPM (apache2 + nginx)

--- vps/domain.sh
+++ vps/domain.sh
@@ -12,13 +12,41 @@ domain() {
   fi
 
   mkdir -p /var/www/$1/{logs,public_html}
+
   cat > /var/www/$1/public_html/default.html <<EOF
-<p>Welcome to the default placeholder for <strong>$1</strong></p>
+<p>Welcome to <strong>$1</strong>'s default placeholder!</p>
 EOF
-  chown www-data:www-data -R /var/www/$1
+  cat > /var/www/$1/public_html/phpInfo.php <<EOF
+<?php phpinfo();
+EOF
 
+  chown root:root -R /var/www/$1
 
-  cat > /etc/nginx/sites-available/$1 <<EOF
+  if [ -e /etc/apache2 ]; then
+
+    cat > /etc/apache2/sites-available/$1 <<EOF
+<VirtualHost *:80>
+  ServerName $1
+  ServerAlias www.$1
+  DocumentRoot /var/www/$1/public_html
+  DirectoryIndex index.html index.php default.html
+  ErrorLog /var/www/$1/logs/error.log
+  CustomLog /var/www/$1/logs/access.log combined
+
+  FastCgiServer /var/www/php5.external -host 127.0.0.1:9000
+  AddHandler php5-fcgi .php
+  Action php5-fcgi /user/lib/cgi-bin/php5.external
+  Alias /user/lib/cgi-bin /var/www/
+
+</VirtualHost>
+EOF
+    a2ensite $1
+
+  fi
+
+  if [ -e /etc/nginx ]; then
+
+    cat > /etc/nginx/sites-available/$1 <<EOF
 server {
   server_name $1;
   listen 80;
@@ -28,12 +56,33 @@ server {
   access_log /var/www/$1/logs/access.log;
   error_log /var/www/$1/logs/error.log;
 
-  index index.html default.html;
+  index index.html index.php default.html;
+
+  location / {
+    try_files $uri $uri/ @rewrites;
+  }
+
+  location @rewrites {
+    rewrite ^ /index.php last;
+  }
+
+  location ~ \.php$ {
+    include /etc/nginx/fastcgi_params;
+    fastcgi_pass 127.0.0.1:9000;
+  }
 }
 EOF
-  ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+    ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
 
-  service nginx restart
+  fi
+
+  if ps -C apache2 > /dev/null; then
+    service apache2 restart
+  fi
+
+  if ps -C nginx > /dev/null; then
+    service nginx restart
+  fi
 }
 
 options=("${options[@]}" "domain <[subdomain.]domain.tld>")
--- vps/lxmp.sh
+++ vps/lxmp.sh
@@ -11,9 +11,13 @@ apache() {
 
   if ! apt_installed apache2; then
 
-    apt_install apache2
-	cp /etc/apache2/conf.d/security /etc/apache2/conf.d/security.bak
+    apt_install 'apache2 libapache2-mod-fastcgi'
+    a2dissite default
+    a2enmod rewrite
+
+    cp /etc/apache2/conf.d/{security,security.bak}
     sed -i 's/^ServerTokens .*/ServerTokens Prod/' /etc/apache2/conf.d/security
+
     ok 'apache installed and setup.\n'
 
   else
@@ -32,20 +36,39 @@ nginx() {
   if ! apt_installed nginx; then
 
     apt_install nginx
-	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+
+    cp /etc/nginx/{nginx.conf,nginx.conf.bak}
     sed -i 's/worker_processes [0-9]*/worker_processes 2/' /etc/nginx/nginx.conf
     sed -i 's/\#\s*server_tokens off;/server_tokens off;/' /etc/nginx/nginx.conf
     sed -i 's/worker_connections [0-9]*/worker_connections 1024/' /etc/nginx/nginx.conf
 
-	service nginx start
+    service nginx start
     ok 'nginx installed and setup.\n'
 
   else
-	update-rc.d nginx defaults
+    update-rc.d nginx defaults
     service nginx start
   fi
 }
 
+phpfpm() {
+  if ! apt_installed php5-fpm; then
+
+    apt_install 'php5-fpm php5-mysql php5-sqlite php5-apc php5-gd php5-mcrypt'
+
+    sed -i 's/^pm.max_children.*/pm.max_children = 8/' /etc/php5/fpm/pool.d/www.conf
+    sed -i 's/^pm.start_servers.*/pm.start_servers = 2/' /etc/php5/fpm/pool.d/www.conf
+    sed -i 's/^pm.min_spare_servers.*/pm.min_spare_servers = 2/' /etc/php5/fpm/pool.d/www.conf
+    sed -i 's/^pm.max_spare_servers.*/pm.max_spare_servers = 4/' /etc/php5/fpm/pool.d/www.conf
+    sed -i 's/\;pm.max_requests.*/pm.max_requests = 1000/' /etc/php5/fpm/pool.d/www.conf
+
+    service php5-fpm restart
+    ok 'php5-fpm installed and setup.\n'
+
+  else
+    service php5-fpm restart
+  fi
+}
 
 mysql() {
   if ! apt_installed mysql; then
@@ -76,11 +99,13 @@ EOF
 lamp() {
   apache
 #  mysql
+  phpfpm
 }
 
 lemp() {
   nginx
 #  mysql
+  phpfpm
 }
 
 options=("${options[@]}" "lamp (Apache/MySQL/PHP)" "lemp (nginx/MySQL/PHP)")
